b7e3baba7fbe23f900ab3b5941a713772afb1d3d
Marek Murawski <marek.murawski@gmail.com>
2013-01-17 15:03:31 +0100

    stable

--- views/suggestions.php
+++ views/suggestions.php
@@ -9,66 +9,105 @@ if (!defined('IN_CMS')) { exit(); }
  * @package     Plugins
  * @subpackage  SEO
  * 
- * @author      Nic Wortel <nic.wortel@nth-root.nl>
+ * @author      Nic Wortel <nd.wortel@gmail.com>
  * @copyright   Nic Wortel, 2012
  * @version     0.2.1
  */
 
 ?>
-<h1><?php echo __('Suggestions'); ?></h1>
+<h1><?php echo __( 'Suggestions' ); ?></h1>
 
-<h2><?php echo __('Meta description'); ?></h2>
+<h2><?php echo __( 'Meta description' ); ?></h2>
 
-<h3><?php echo __('Missing meta descriptions'); ?></h3>
-
-<ul>
-    <?php foreach ($suggestions['description']['missing'] as $suggestion): ?>
-    <li><a href="<?php echo get_url('page/edit', $suggestion->id); ?>"><?php echo $suggestion->title; ?></a></li>
+<h3><?php echo __( 'Missing meta descriptions' ); ?></h3>
+<table>
+    <?php foreach ( $suggestions['description']['missing'] as $suggestion ): ?>
+        <tr>
+            <td>
+                <a href="<?php echo get_url( 'page/edit', $suggestion->id ); ?>"><?php echo $suggestion->title; ?></a>
+            </td>
+            <td class="actions"> 
+                <a href="<?php echo URL_PUBLIC . $suggestion->getUri(); ?>" target="_blank">[<?php echo __( 'visit' ); ?>]</a>
+            <td>
+        </tr>
     <?php endforeach; ?>
-</ul>
+</table>
 
-<h3><?php echo __('Duplicate meta descriptions'); ?></h3>
+<h3><?php echo __( 'Duplicate meta descriptions' ); ?></h3>
 
-<h3><?php echo __('Long meta descriptions'); ?></h3>
-
-<ul>
-    <?php foreach ($suggestions['description']['long'] as $suggestion): ?>
-    <li><a href="<?php echo get_url('page/edit', $suggestion->id); ?>"><?php echo $suggestion->title; ?></a></li>
+<h3><?php echo __( 'Long meta descriptions' ); ?></h3>
+<table>
+    <?php foreach ( $suggestions['description']['long'] as $suggestion ): ?>
+        <tr>
+            <td>
+                <a href="<?php echo get_url( 'page/edit', $suggestion->id ); ?>"><?php echo $suggestion->title; ?></a>
+            </td>
+            <td class="actions">
+                <a href="<?php echo URL_PUBLIC . $suggestion->getUri(); ?>" target="_blank">[<?php echo __( 'visit' ); ?>]</a>
+            </td>
+        </tr>
     <?php endforeach; ?>
-</ul>
+</table>
 
-<h3><?php echo __('Short meta descriptions'); ?></h3>
+<h3><?php echo __( 'Short meta descriptions' ); ?></h3>
 
-<ul>
-    <?php foreach ($suggestions['description']['short'] as $suggestion): ?>
-    <li><a href="<?php echo get_url('page/edit', $suggestion->id); ?>"><?php echo $suggestion->title; ?></a></li>
+<table>
+    <?php foreach ( $suggestions['description']['short'] as $suggestion ): ?>
+        <tr>
+            <td>
+                <a href="<?php echo get_url( 'page/edit', $suggestion->id ); ?>"><?php echo $suggestion->title; ?></a>
+            </td>
+            <td class="actions">
+                <a href="<?php echo URL_PUBLIC . $suggestion->getUri(); ?>" target="_blank">[<?php echo __( 'visit' ); ?>]</a>
+            </td>
+        </tr>
     <?php endforeach; ?>
-</ul>
+</table>
 
-<h2><?php echo __('Page titles'); ?></h2>
+<h2><?php echo __( 'Page titles' ); ?></h2>
 
-<h3><?php echo __('Missing titles'); ?></h3>
-
-<ul>
-    <?php foreach ($suggestions['title']['missing'] as $suggestion): ?>
-    <li><a href="<?php echo get_url('page/edit', $suggestion->id); ?>"><?php echo $suggestion->title; ?></a></li>
+<h3><?php echo __( 'Missing titles' ); ?></h3>
+<table>
+    <?php foreach ( $suggestions['title']['missing'] as $suggestion ): ?>
+        <tr>
+            <td>
+                <a href="<?php echo get_url( 'page/edit', $suggestion->id ); ?>"><?php echo $suggestion->title; ?></a>
+            </td>
+            <td class="actions">
+                <a href="<?php echo URL_PUBLIC . $suggestion->getUri(); ?>" target="_blank">[<?php echo __( 'visit' ); ?>]</a>
+            </td>
+        </tr>
     <?php endforeach; ?>
-</ul>
+</table>
 
-<h3><?php echo __('Duplicate titles'); ?></h3>
+<h3><?php echo __( 'Duplicate titles' ); ?></h3>
 
-<h3><?php echo __('Long titles'); ?></h3>
+<h3><?php echo __( 'Long titles' ); ?></h3>
 
-<ul>
-    <?php foreach ($suggestions['title']['long'] as $suggestion): ?>
-    <li><a href="<?php echo get_url('page/edit', $suggestion->id); ?>"><?php echo $suggestion->title; ?></a></li>
+<table>
+    <?php foreach ( $suggestions['title']['long'] as $suggestion ): ?>
+        <tr>
+            <td>
+                <a href="<?php echo get_url( 'page/edit', $suggestion->id ); ?>"><?php echo $suggestion->title; ?></a>
+            </td>
+            <td class="actions">
+                <a href="<?php echo URL_PUBLIC . $suggestion->getUri(); ?>" target="_blank">[<?php echo __( 'visit' ); ?>]</a>
+            </td>
+        </tr>
     <?php endforeach; ?>
-</ul>
+</table>
 
-<h3><?php echo __('Short titles'); ?></h3>
+<h3><?php echo __( 'Short titles' ); ?></h3>
 
-<ul>
-    <?php foreach ($suggestions['title']['short'] as $suggestion): ?>
-    <li><a href="<?php echo get_url('page/edit', $suggestion->id); ?>"><?php echo $suggestion->title; ?></a></li>
+<table>
+    <?php foreach ( $suggestions['title']['short'] as $suggestion ): ?>
+        <tr>
+            <td>
+                <a href="<?php echo get_url( 'page/edit', $suggestion->id ); ?>"><?php echo $suggestion->title; ?></a>
+            </td>
+            <td class="actions">
+                <a href="<?php echo URL_PUBLIC . $suggestion->getUri(); ?>" target="_blank">[<?php echo __( 'visit' ); ?>]</a>
+            </td>
+        </tr>
     <?php endforeach; ?>
-</ul>
+</table>
